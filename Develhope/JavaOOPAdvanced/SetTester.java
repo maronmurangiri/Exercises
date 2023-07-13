7cc9a0328aad924ac2391a7ec1e1f7d1fb4e6cd7
Maron <murangirimaron@gmail.com>
2023-07-13 15:18:50 +0300

    Added SetTester.java

---
+++ Develhope/JavaOOPAdvanced/SetTester.java
@@ -0,0 +1,34 @@
+package Develhope.JavaOOPAdvanced;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetTester {
+    public static void main(String[] args){
+        Set<String> daysOfTheWeek = new HashSet<>();
+                daysOfTheWeek.add("Monday");
+                daysOfTheWeek.add("Tuesday");
+                daysOfTheWeek.add("Wednesday");
+                daysOfTheWeek.add("Thursday");
+                daysOfTheWeek.add("Friday");
+                daysOfTheWeek.add("Saturday");
+                daysOfTheWeek.add("Sunday");
+
+        System.out.println(daysOfTheWeek);
+
+        Set<String> orderedDaysOfTheWeek = new LinkedHashSet<>();
+                 orderedDaysOfTheWeek.add("Monday");
+                 orderedDaysOfTheWeek.add("Tuesday");
+                 orderedDaysOfTheWeek.add("Wednesday");
+                 orderedDaysOfTheWeek.add("Thursday");
+                 orderedDaysOfTheWeek.add("Friday");
+                 orderedDaysOfTheWeek.add("Saturday");
+                 orderedDaysOfTheWeek.add("Sunday");
+
+        System.out.println(orderedDaysOfTheWeek);
+
+        System.out.println("Is the first Set equal to the second one? "+ (orderedDaysOfTheWeek.equals(daysOfTheWeek)?"Yes":"No"));
+
+    }
+}
