5ef0f6e3e877bc804374b7813dc19b0ea917aa52
Maron <murangirimaron@gmail.com>
2023-07-14 11:36:30 +0300

    Added file ExceptionHandling.java

---
+++ Develhope/JavaOOPAdvanced/ExceptionHandling.java
@@ -0,0 +1,23 @@
+package Develhope.JavaOOPAdvanced;
+
+public class ExceptionHandling {
+    public static void checkScore(double score){
+        if (score>0 && score<=50.00){
+            System.out.println("Average Score");
+        } else if (score>50.00 && score<=100) {
+            System.out.println("Very good score");
+        }
+        else {
+
+            throw new ArithmeticException("Score is out of scale");
+
+        }
+    }
+
+    public static void main(String[] args){
+        checkScore(2.15);
+        checkScore(50);
+        checkScore(95);
+        checkScore(100.01);
+    }
+}
