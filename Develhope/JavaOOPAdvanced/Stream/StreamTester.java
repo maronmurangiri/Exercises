067e6d1406e0e10bb3dd43b9fb454bb4bb16be20
Maron <murangirimaron@gmail.com>
2023-07-20 18:28:45 +0300

    commited the stream file

---
+++ Develhope/JavaOOPAdvanced/Stream/StreamTester.java
@@ -0,0 +1,45 @@
+package Develhope.JavaOOPAdvanced.Stream;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+class WriteAFile{
+    public void writingAFile(){
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("Develhope/JavaOOPAdvanced/Stream/file.txt");
+            writer.write("hello\n");
+            writer.write("how\n");
+            writer.write("are you\n");
+            writer.write("?");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
+public class StreamTester {
+    public static void main(String[] args) {
+        WriteAFile writeAFile = new WriteAFile();
+                writeAFile.writingAFile();
+
+
+        Path filePath =Path.of("Develhope/JavaOOPAdvanced/Stream/file.txt");
+        try {
+            Stream<String> fileLines = Files.lines(filePath);
+            fileLines.forEach(lines-> System.out.println(lines));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    }
+
+
+
+
