4abc1843a8ef3540d3d25c635b8272587e028a92
Maron <murangirimaron@gmail.com>
2023-08-07 18:04:56 +0300

    User authentication CRUD operations

---
+++ src/main/java/authentication/UserAuthCRUDOperations.java
@@ -0,0 +1,104 @@
+package authentication;
+
+import authentication.entity.User;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import java.sql.Timestamp;
+import java.util.List;
+
+import positions.entity.Position;
+import role.entity.Role;
+
+public class UserAuthCRUDOperations {
+
+    //Creating entity manager factory object
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("DbConnect");
+
+
+    public void insertUser(String firstName, String surname,String email, String password, Position position, Role role, int createdBy, Timestamp createdOn){
+
+        //obtaining entity manager from the entity manager factory
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        //create entity transaction object and begin transaction
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        try{
+            User user = new User();
+            user.setFirstname(firstName);
+            user.setSurname(surname);
+            user.setEmail(email);
+            user.setPassword(password);
+            user.setPosition(position);
+            user.setRole(role);
+            user.setCreatedBy(createdBy);
+            user.setCreatedOn(createdOn);
+
+            //persist the user instance
+            entityManager.persist(user);
+
+            //commit transaction
+            entityTransaction.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        entityManager.close();
+
+    }
+
+
+    public Integer findUserByEmail(String email){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Query query = entityManager.createQuery("SELECT u.id FROM User u WHERE u.email = :email");
+        query.setParameter("email",email);
+
+        List<Integer> users = query.getResultList();
+        if (!users.isEmpty()) {
+            for (int userID : users){
+                return userID;
+            }
+
+        }
+        return  null;
+    }
+
+    public User findUSerByRoleID(Role roleId){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Query query = entityManager.createQuery("SELECT u FROM User u WHERE u.role = :roleId");
+        query.setParameter("roleId",roleId);
+
+        List<User> users = query.getResultList();
+        for (User user : users){
+            return user;
+        }
+        return null;
+    }
+
+
+    public void updateUser(User userToUpdate,String password){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        userToUpdate.setPassword(password);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+
+    public void deleteUser(User userToDelete){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        entityManager.remove(userToDelete);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+
+    }
+}
