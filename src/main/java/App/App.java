435872e81114ab37f36daa0ffd66692bb6aeaa77
Maron <murangirimaron@gmail.com>
2023-09-04 17:35:02 +0300

    updated

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -29,21 +29,5 @@ public class App {
             throw new RuntimeException(e);
         }
 
-
-        // ticketCRUDOperations.insertTicket("No cart", "Cart disappears after sometime",ticketStatus,"High",createdOn, createdBy, Timestamp.valueOf("2023-09-25 21:23:33.564"));
-//        ticketCRUDOperations.insertTicket("Discount does not reflect", "Discount does not reflect on total cost",
-//                "medium", Timestamp.valueOf("2023-09-25 21:23:33.564"));
-//        ticketCRUDOperations.insertTicket("Transaction timeouts", "Transaction takes too long to complete",
-//                "high",1, Timestamp.valueOf("2023-08-25 21:23:33.564"));
-//
-//        ticketCRUDOperations.insertTicket("Transaction errors out", "Transaction err  ors out after 2 seconds",101,
-//                "high", Timestamp.valueOf("2023-08-30 16:03:33.564"));
-
-
-        /*ticketCRUDOperations.updateTicketDeadlineDate(1, Timestamp.valueOf("2023-10-25 21:23:33.564"));
-        Ticket ticketToUpdate = ticketCRUDOperations.findTicketById(4);
-        System.out.println(ticketToUpdate.toString());*/
-
-//        ticketCRUDOperations.deleteTicket(5);
     }
 }
