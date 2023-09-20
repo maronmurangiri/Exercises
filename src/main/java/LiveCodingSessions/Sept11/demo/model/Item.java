229fdcfa4c19a6deed9380700ffa69dc4577fb55
Maron <murangirimaron@gmail.com>
2023-09-20 11:47:18 +0300

    added lombok to the Item and Task entities of sept 14th livecoding

--- src/main/java/LiveCodingSessions/Sept11/demo/model/Item.java
+++ src/main/java/LiveCodingSessions/Sept11/demo/model/Item.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Item {
 
@@ -25,27 +27,4 @@ public class Item {
     public Item() {
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/main/java/LiveCodingSessions/sept14th/mvcQuiz1/model/Task.java
+++ src/main/java/LiveCodingSessions/sept14th/mvcQuiz1/model/Task.java
@@ -6,13 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Data
-@Getter
-@Setter
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Task {
     @Id
@@ -30,27 +26,4 @@ public class Task {
         this.completionStatus = completionStatus;
     }
 
-    public Integer getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(Integer taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCompletionStatus() {
-        return completionStatus;
-    }
-
-    public void setCompletionStatus(String completionStatus) {
-        this.completionStatus = completionStatus;
-    }
 }
